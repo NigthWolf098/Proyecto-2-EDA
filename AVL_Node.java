4858a46fe62b2823792874f99169c07f9634877c
Merida Serralde Francisco Jared <142857260+NigthWolf098@users.noreply.github.com>
2024-11-09 21:51:51 -0600

    Update AVL_Node.java

--- AVL_Node.java
+++ AVL_Node.java
@@ -11,24 +11,4 @@ public class AVL_Node {
         this.izq = null;
         this.der = null;
     }
-
-    // Calcula la altura del subárbol desde este nodo
-    public int Altura() {
-        
-        int alturaIzq = 0;
-        if(this.izq != null){
-            this.izq.Altura();
-        }else{
-            alturaIzq = 0;
-        }
-        
-        int alturaDer = 0;
-        if(this.der != null){
-            this.der.Altura();
-        }else{
-            alturaDer = 0;
-        }
-        return 1 + Math.max(alturaIzq, alturaDer);
-        
-    }
 }
