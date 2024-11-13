6e6bd06b15e12689af9ca8064c4854580758b7d4
JimenoCorderoManuel <142749821+JimenoCorderoManuel@users.noreply.github.com>
2024-11-12 21:52:46 -0600

    Update MenuUsuario.java

--- MenuUsuario.java
+++ MenuUsuario.java
@@ -20,7 +20,43 @@ public class MenuUsuario {
                 break;
             
             case '2' :
-                
+                 Arbol_Heap heap= new Arbol_Heap();
+                System.out.println("**Empezemos a crear tu Arbol Heap**");
+                String option;
+                do{
+                        System.out.println("1.-Insertar Valor \n 2.-Eliminar Raiz \n "
+                                + "3.-Imprimir recorrido PreOrden \n 4.- Regresar al menu");
+                        System.out.print("Ingresa la opcion que desees realizar: ");
+                        option = scanner.nextLine();
+                        switch(option){
+                          
+                            case "1" :
+                                    int valor;
+                                    System.out.print("Ingresa el valor(entero) del nodo a ingresar:");
+                                    valor = scanner.nextInt();
+                                    heap.insertar(valor);
+                                    System.out.println("Valor "+valor+" ingresado correctamente");
+                                break;
+                            case "2" :
+                                  System.out.println("La raiz "+heap.getRoot()+" ha sido eliminada");
+                                  heap.eliminarRaiz();
+                                break;
+                            
+                            case "3" :
+                                System.out.println("Recorrido PreOrden de tu arbol Heap: ");
+                                heap.preOrden(heap.getRoot());
+                                
+                                break;
+                                
+                            case "4" :
+                                System.out.println("Regresando al Menu de Opciones...");
+                                break;
+                                
+                            default :
+                                System.out.println("Opcion incorrecta");
+                                break;
+                        }
+                    }while(option != "4");
                 break;
                 
             case '3' :
